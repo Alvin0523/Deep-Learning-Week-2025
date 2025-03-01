c26888ad09a6911a6b7670e1192959e72071a087
frieddeli <rayshao2002@gmail.com>
2025-03-01 17:53:50 +0800

    removed extra files

--- app/TTS.py
+++
@@ -1,8 +0,0 @@
-import pyttsx3
-
-def tts_init(): 
-    engine = pyttsx3.init()
-    #text to speech settings
-    engine.setProperty('rate', 100)     # Speed percent (can go over 100)
-    engine.setProperty('volume', 1.0)  # Volume 0-1
-    return engine
