0b39298d83f6d81d25b926bb35151c9d2c46a34a
Wei Ming <alvinwm0523@gmail.com>
2025-03-01 15:33:17 +0800

    added nlp place holder

--- app/vision.py
+++ app/vision.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 model = YOLO(r"C:\Users\alvin\OneDrive - Nanyang Technological University\Files\03_Work\05_Hackathon\Deep Learning Week 2025\model\best.pt")
 
 # Confidence threshold
-CONFIDENCE_THRESHOLD = 0.5
+CONFIDENCE_THRESHOLD = 0.7
 
 def get_frame(cap):
     """Captures a frame from the webcam, performs YOLOv8 detection, and returns the processed frame as an RGB numpy array."""
@@ -40,3 +40,15 @@ def get_frame(cap):
     frame_rgb = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB)
 
     return frame_rgb
+
+def is_object_detected(frame_resized):
+    """Runs YOLOv8 detection on a frame and returns True if an object is detected, otherwise False."""
+
+    results = model(frame_resized)[0]
+
+    for box in results.boxes:
+        conf = box.conf[0].item()
+        if conf >= CONFIDENCE_THRESHOLD:
+            return True  # Object detected
+
+    return False  # No object detected
